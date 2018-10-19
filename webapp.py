02253686f6f5a6376c9e6b820ab2d3e9888b01b1
TarheK <42652896+TarheK@users.noreply.github.com>
2018-10-19 10:29:36 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,22 +5,39 @@ app = Flask(__name__)
 @app.route("/")
 def render_home():
     return render_template('home.html')
+@app.route("/responseh")
+def render_responseh():
+    millimeters = float(request.args['millimeters'])
+    response=millimeters * (1/10)
+    return rendr_template('response = response)
 
 @app.route("/page1")
 def render_page1():
-    return render_template('page1.html, response = response')
+    return render_template('page1.html')
+@app.route("/response1")
 def render_response1():
     feet = float(request.args['feet'])
-    response=feet * (1/12)
+    response=feet * 12
+    return rendr_template('response = response)
     
 
 @app.route("/page2")
 def render_page2():
     return render_template('page2.html')
+@app.route("/response2")
+def render_response2():
+    killometers = float(request.args['killometers'])
+    response=killometers * 1000
+    return rendr_template('response = response)                       
 
 @app.route("/page3")
 def render_page3():
     return render_template('page3.html')
+@app.route("/response3)
+def render_response3):
+   miles float(request.args['miles'])
+    response=mile * 5280
+    return rendr_template('response = response)
 
     
 if __name__=="__main__":
